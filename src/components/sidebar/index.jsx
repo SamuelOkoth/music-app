26625fd6c92845ba8d5ffe33eeba04a6afaa24ee
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-19 11:51:07 +0300

    added component to the home

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
-export default function index() {
+import "./sidebar.css"
+export default function Sidebar() {
   return (
-    <div>
-      
+    <div className='sidebar-container'>
+      Sidebar
     </div>
   )
 }
