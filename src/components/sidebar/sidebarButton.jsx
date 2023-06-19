75d54a700cbc0e809438c3d86e48a4b087e902ab
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-19 23:34:14 +0300

    routing done

--- src/components/sidebar/sidebarButton.jsx
+++ src/components/sidebar/sidebarButton.jsx
@@ -1,13 +1,20 @@
-import "./sidebarButton.css";
-import { Link } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import "./sidebarButton.css";
 
 export default function SidebarButton(props) {
+  const { to } = props;
+
   return (
     <div>
-      <Link to={props.to}>
-      
+      <Link to={to}>
+        {props.to}
       </Link>
     </div>
-  )
+  );
 }
+
+SidebarButton.propTypes = {
+  to: PropTypes.string.isRequired,
+};
