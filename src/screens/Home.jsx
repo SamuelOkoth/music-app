a0ea18a3a920925692e5740ca50c16fc6c0017de
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-19 11:35:50 +0300

    created home.css to style teh body

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Player from './player'
+import Trending from './trending'
+import Feed from './feed'
+import Library from './library'
+import Favorites from './favorites'
+import "./home.css"
 
 export default function Home() {
   return (
-    <Router>
+    <Router className="main-body">
+        <div>
         <Routes>
-            <Route path="/" element={<Library />} />
-        </Routes>
+        <Route path="/library" element={<Library />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+        </div>
+      
     </Router>
   )
- }
+}
