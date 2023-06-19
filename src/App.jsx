0f2ec4779d0e6fcd157535b6cb7757fe50517025
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-19 10:58:08 +0300

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './screens/Home'
 
 export default function App() {
   return (
-    <div>
-      Muzika 
-    </div>
+<div>
+    <Home/>    
+
+</div>
   )
 }
