8861d8185436370ad2f568656c01e0c7ed489e20
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-19 10:41:58 +0300

    created a functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <div>
       
-    </>
+    </div>
   )
 }
-
-export default App
