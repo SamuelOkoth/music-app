73b76076780d8b83627b2f8781d76fd55ac7e327
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-21 09:15:39 +0300

    created auth login

---
+++ src/spotify.jsx
@@ -0,0 +1,11 @@
+const authEndpoint = "http://accounts.spotify.com/authorize?"
+const clientId = "914d7fb580ea48c19610e967a3a99ec4"
+const redirectUri = "http://localhost:3000"
+const scopes = ["user-library-read", "playlist-read-private"];
+
+
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
+  
+  
