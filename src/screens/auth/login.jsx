11b900c1c957478d8e80cc8fce59bf9109ff8864
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-21 09:31:35 +0300

    started consuming spotify's api

---
+++ src/screens/auth/login.jsx
@@ -0,0 +1,12 @@
+import { loginEndpoint } from "../../spotify"
+import "./login.css"
+export default function Login() {
+  return (
+    <div>
+      <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+       alt="spotify logo" className="logo"/>
+        <a href=""></a>
+
+    </div>
+  )
+}
