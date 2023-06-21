de72e996222da56c5d8566208bed4f5cb7214638
SamuelOkoth <okothsamuel78@gmail.com>
2023-06-21 09:48:26 +0300

    changed the redirect uri for spotify

--- src/screens/auth/login.jsx
+++ src/screens/auth/login.jsx
@@ -6,7 +6,10 @@ export default function Login() {
       <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
        alt="spotify logo" className="logo"/>
         <a href={loginEndpoint}>
+            <button>
             <div className="logo-btn">LOGIN</div>
+            </button>
+            
         </a>
 
     </div>
