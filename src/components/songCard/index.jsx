5dd50cd118a929428f4b4669c6eb9aa72ecfd9ee
SamuelOkoth <okothsamuel78@gmail.com>
2023-07-03 03:12:13 -0600

    added thesongcard

---
+++ src/components/songCard/index.jsx
@@ -0,0 +1,12 @@
+import AlbumImage from "./albumImage";
+import AlbumInfo from "./albumInfo";
+import "./songCard.css";
+
+export default function SongCard({ album }) {
+  return (
+    <div className="songCard-body flex">
+      <AlbumImage url={album?.images[0]?.url} />
+      <AlbumInfo album={album} />
+    </div>
+  );
+}
